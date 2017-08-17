f2ecb3875bacea38fceee1fecc216fe1d2ea4884
Gabe Tiller <gabectiller@gmail.com>
2017-08-17 18:32:32 -0400

    assignent 9 complete

---
+++ scripts/utilities.js
@@ -0,0 +1,10 @@
+// forEach should take two arguments, an array and a function
+// and then call the function on each element in the array
+function forEach(array, funct) {
+  for (var i=0; i<array.length; i++){
+    funct(array[i]);
+  }
+}
+
+// cmd + [ cmd + ] - indenting
+// cmd + d – select word, run again to select next copy of word
