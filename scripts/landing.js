672321369123af45c9f15fd24bc891bfce004aac
Gabe Tiller <gabectiller@gmail.com>
2017-08-16 23:50:11 -0400

    checkpoint-9-completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,15 +2,30 @@
 // Refactor the individual style calls of the landing.js script to be a single function named revealPoint that:
 // takes a single argument: the index of the points class node element, and
 // gets called in a for loop.
+var pointsArray = document.getElementsByClassName('point');
 
-var revealPoint = function() {
-var points = document.getElementsByClassName('point');
-for (var i=0; i<points.length; i++) {
-  points[i].style.opacity = 1;
-  points[i].style.transform = "scaleX(1) translateY(0)";
-  points[i].style.msTransform = "scaleX(1) translateY(0)";
-  points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function(points) {
+  var revealPoint = function(index) {
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].style.msTransform = "scaleX(1) translateY(0)";
+    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
 
+    }
+
+  for (var i = 0; i < points.length; i++) {
+  revealPoint(i);
   }
+};
 
+window.onload = function() {
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener("scroll", function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+         }
+
+  });
 }
