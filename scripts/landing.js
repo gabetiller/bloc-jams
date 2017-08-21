39d63b5c8b903a705d8280bb42d31899c1acf8a5
Gabe Tiller <gabectiller@gmail.com>
2017-08-21 12:11:31 -0400

    checkpoint 16 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,32 +2,31 @@
 // Refactothe individual style calls of the landing.js script to be a single function named revealPoint that:
 // takes a single argument: the index of the points class node element, and
 // gets called in a for loop.
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
-  var revealPoint = function(point) {
-    point.style.opacity = 1;
-    point.style.transform = "scaleX(1) translateY(0)";
-    point.style.msTransform = "scaleX(1) translateY(0)";
-    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function() {
 
     }
+var revealPoint = function() {
+         // #7
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+    };
 
-  forEach(points, revealPoint);
+  $.each($('.point'), revealPoint);
 };
 
-window.onload = function() {
-    if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+$(window).load(function() {
+  if ($(window).height() > 950) {
+        animatePoints();
      }
 
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-     window.addEventListener("scroll", function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);
+        $(window).scroll(function(event) {
+          if ($(window).scrollTop() >= scrollDistance) {
+              animatePoints();
          }
 
   });
-}
+});
